470a5e8806f58ed86680ef40c669e067bad88fbd
Simon Elliston Ball <simon@simonellistonball.com>
2017-12-20 02:42:13 +0000

    Cleaned up whitespace for checkstyle

--- nifi-netflow-processors/src/main/java/com/simonellistonball/nifi/processors/netflow/NetflowReader.java
+++ nifi-netflow-processors/src/main/java/com/simonellistonball/nifi/processors/netflow/NetflowReader.java
@@ -38,28 +38,29 @@ import org.apache.nifi.serialization.record.RecordSchema;
 @CapabilityDescription("Reads packets in Cisco Netflow 5, 9 and IPFIX formats and outputs a Record representation")
 public class NetflowReader extends AbstractControllerService implements RecordReaderFactory {
 
-	static final AllowableValue NETFLOW_5 = new AllowableValue("netflow-5", "Netflow 5", "Netflow v5");
-	static final AllowableValue NETFLOW_9 = new AllowableValue("netflow-9", "Netflow 9", "Netflow v9");
-	static final AllowableValue IPFIX = new AllowableValue("ipfix", "IPFIX", "IPFIX");
-	static final AllowableValue NETFLOW_ANY = new AllowableValue("any", "Any", "Parse any format we receive based on the version in the header");
+    static final AllowableValue NETFLOW_5 = new AllowableValue("netflow-5", "Netflow 5", "Netflow v5");
+    static final AllowableValue NETFLOW_9 = new AllowableValue("netflow-9", "Netflow 9", "Netflow v9");
+    static final AllowableValue IPFIX = new AllowableValue("ipfix", "IPFIX", "IPFIX");
+    static final AllowableValue NETFLOW_ANY = new AllowableValue("any", "Any",
+            "Parse any format we receive based on the version in the header");
 
-	static final PropertyDescriptor NETFLOW_VERSION = new PropertyDescriptor.Builder().name("netflow-version")
-			.displayName("Netflow Version").description("Which version of netflow should the reader parse?")
-			.allowableValues(NETFLOW_5, NETFLOW_9, IPFIX, NETFLOW_ANY).defaultValue(NETFLOW_ANY.getValue())
-			.required(true).build();
+    static final PropertyDescriptor NETFLOW_VERSION = new PropertyDescriptor.Builder().name("netflow-version")
+            .displayName("Netflow Version").description("Which version of netflow should the reader parse?")
+            .allowableValues(NETFLOW_5, NETFLOW_9, IPFIX, NETFLOW_ANY).defaultValue(NETFLOW_ANY.getValue())
+            .required(true).build();
 
-	@Override
-	protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-		final List<PropertyDescriptor> properties = new ArrayList<>(super.getSupportedPropertyDescriptors());
-		properties.add(NETFLOW_VERSION);
-		return properties;
-	}
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        final List<PropertyDescriptor> properties = new ArrayList<>(super.getSupportedPropertyDescriptors());
+        properties.add(NETFLOW_VERSION);
+        return properties;
+    }
 
-	@Override
-	public RecordReader createRecordReader(Map<String,String> flowFile, InputStream in, ComponentLog logger)
-			throws MalformedRecordException, IOException, SchemaNotFoundException {
-		final RecordSchema schema = null;
-		return new NetflowRecordReader(in, schema, logger);
-	}
+    @Override
+    public RecordReader createRecordReader(Map<String, String> flowFile, InputStream in, ComponentLog logger)
+            throws MalformedRecordException, IOException, SchemaNotFoundException {
+        final RecordSchema schema = null;
+        return new NetflowRecordReader(in, schema, logger);
+    }
 
 }
