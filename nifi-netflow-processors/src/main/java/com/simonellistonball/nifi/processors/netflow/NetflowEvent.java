bbae0819bc63044875ec2ce9ee52cc3c73843113
Simon Elliston Ball <simon@simonellistonball.com>
2017-12-20 02:28:57 +0000

    Clean up Version fix

--- nifi-netflow-processors/src/main/java/com/simonellistonball/nifi/processors/netflow/NetflowEvent.java
+++
@@ -1,5 +0,0 @@
-package com.simonellistonball.nifi.processors.netflow;
-
-import org.apache.nifi.processor.util.listen.event.StandardEvent;
-import org.apache.nifi.processor.util.listen.response.ChannelResponder;
-
--- nifi-netflow-processors/src/main/java/com/simonellistonball/nifi/processors/netflow/NetflowReader.java
+++ nifi-netflow-processors/src/main/java/com/simonellistonball/nifi/processors/netflow/NetflowReader.java
@@ -1,16 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.simonellistonball.nifi.processors.netflow;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.AllowableValue;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.controller.AbstractControllerService;
-import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.schema.access.SchemaNotFoundException;
 import org.apache.nifi.serialization.MalformedRecordException;
@@ -40,7 +56,7 @@ public class NetflowReader extends AbstractControllerService implements RecordRe
 	}
 
 	@Override
-	public RecordReader createRecordReader(FlowFile flowFile, InputStream in, ComponentLog logger)
+	public RecordReader createRecordReader(Map<String,String> flowFile, InputStream in, ComponentLog logger)
 			throws MalformedRecordException, IOException, SchemaNotFoundException {
 		final RecordSchema schema = null;
 		return new NetflowRecordReader(in, schema, logger);
